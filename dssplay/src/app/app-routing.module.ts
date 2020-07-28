53d4c5e8beea1381830b94b8bb457c0a43514e01
AnthonyRawlinsUoM <rawlinsfamily@icloud.com>
2020-07-28 13:27:12 +1000

    update routing

--- dssplay/src/app/app-routing.module.ts
+++ dssplay/src/app/app-routing.module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
-import { MappingComponent } from './mapping/mapping.component';
-import { ChartingComponent } from './charting/charting.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { UserRoleChooserComponent } from './user-role-chooser/user-role-chooser.component';
 import { UnifiedReportComponent } from './unified-report/unified-report.component';
 
 
